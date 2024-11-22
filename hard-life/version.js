f293ce713df6ea669a360bd10e145a2a4d0e1b3c
slopegames86 <celeiace32xclusive@gmail.com>
2024-11-22 15:53:51 +0700

    upload

---
+++ hard-life/version.js
@@ -0,0 +1,2 @@
+version="3";
+libs=["phaser-ads.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-spine@3.0/build/phaser-spine.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-super-storage@1.0/build/phaser-super-storage.min.js","splash.min.js"];
